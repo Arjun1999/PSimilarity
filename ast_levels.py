38d2a8aee4d1033e05e294268cc3de705845d33a
prateksha <pratekshau@gmail.com>
2020-06-02 22:44:39 +0530

    AST preprocessing

--- ast_levels.py
+++ ast_levels.py
@@ -1,4 +1,20 @@
 import ast
+from astor import to_source
+
+class MyNodeTransformer(ast.NodeTransformer):
+    def visit_Name(self, node):
+        if(isinstance(node, ast.Name)):
+            result = ast.Name(id='x')
+            return ast.copy_location(result, node)
+        return node
+
+def mutate(filename):
+    file = open(filename)
+    contents = file.read()
+    parsed = ast.parse(contents)
+    nodeVisitor = MyNodeTransformer()
+    transformed = nodeVisitor.visit(parsed)
+    return ast.parse(to_source(transformed))
 
 level0 = []
 level1 = []
@@ -19,10 +35,14 @@ def ast_print(node, level=0):
         elif isinstance(value, ast.AST):
             ast_print(value, level=level+1)
 
-file1 = open('sample2.py')
-contents1 = file1.read()
-parsed1 = ast.parse(contents1)
-ast_print(parsed1)
+filename = 'sample2.py'
+input_tree = mutate(filename)
+ast_print(input_tree)
+
+level0 = sorted(level0)
+level1 = sorted(level1)
+level2 = sorted(level2)
+
 print("LEVEL0 = ", level0)
 print("LEVEL1 = ", level1)
 print("LEVEL2 = ", level2)
