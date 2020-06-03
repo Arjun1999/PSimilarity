7019dcc4a79fccce1d4bff1a2e9f65848f2aae6b
prateksha <pratekshau@gmail.com>
2020-06-04 00:16:55 +0530

    Minor fix

--- Proposed_Work/PythonAssignment/6_Prateksha.py
+++ Proposed_Work/PythonAssignment/6_Prateksha.py
@@ -6,22 +6,21 @@ def ensure(x,y):
 
 #Computes the greatest common divisor.
 def gcd(x,y):
-	if ensure(x,y)==True:
-		if (x!=0 and y==0):
-			return x
-  
-		elif(x%y==0):
-			return y
-		else:
-			return gcd(y,x%y)
+    if ensure(x,y)==True:
+        if (x!=0 and y==0):
+            return x 
+        elif(x%y==0):
+            return y
+        else:
+            return gcd(y,x%y)
 			
- 	elif (x==y):
-		if(x==0 and y==0):
-			return None
-  		else:
-   			return x
- 	else:
- 		 print ("Please enter the numbers in correct order.")
+    elif (x==y):
+        if(x==0 and y==0):
+            return None
+        else:
+            return x
+    else:
+        print ("Please enter the numbers in correct order.")
 
 a=int(input("Enter x "))
 b=int(input("Enter y "))
