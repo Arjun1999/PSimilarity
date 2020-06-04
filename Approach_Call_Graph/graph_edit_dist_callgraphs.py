cc806ef3dfb3674ed7602a19d38ad438ae04e7aa
Arjun Verma <arjun.verma@iiitb.org>
2020-06-04 08:50:12 +0530

    updated ged

--- Approach_Call_Graph/graph_edit_dist_callgraphs.py
+++ Approach_Call_Graph/graph_edit_dist_callgraphs.py
@@ -1,6 +1,6 @@
 import networkx as nx
 # import pygraphviz
-import gmatch4py as gm
+# import gmatch4py as gm
 import sys
 
 filename1 = sys.argv[1]
@@ -14,9 +14,9 @@ ged = nx.graph_edit_distance(G1,G2)
 print("Graph Edit Distance from Networkx : ", ged)
 
 # gmatch4py
-ged = gm.GraphEditDistance(1,1,1,1) # node insertion cost,node deletion cost, edge insertion cost, edge deletion cost
-result = ged.compare([G1,G2],None) 
-print("Graph Edit Distance Matrix from GMatch4py : ", result)
+# ged = gm.GraphEditDistance(1,1,1,1) # node insertion cost,node deletion cost, edge insertion cost, edge deletion cost
+# result = ged.compare([G1,G2],None) 
+# print("Graph Edit Distance Matrix from GMatch4py : ", result)
 
 # A sufficient working prototype using the libraries has been implemented.
 # However, a self defined graph edit distance can be implemented to not depend much on libraries.
